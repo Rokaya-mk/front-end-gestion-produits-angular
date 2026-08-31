88501c8ef0e0e7691102fa5b13a8c827a97f2c0c
Rokaya-mk <mbairikrqia@gmail.com>
2026-08-31 16:19:08 +0100

    implementer OnInit dans component products

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
-export class Products {}
+export class Products  implements OnInit {
+  products! : Array<any>;
+
+  constructor() {}
+
+  ngOnInit(): void {
+      this.products =  [
+          { id:1,name: "Product 1", price: 100.99, selected: true },
+          { id:2,name: "Product 2", price: 200.00, selected: true },
+          { id:3,name: "Product 3", price: 150.00, selected: false },
+        ]
+  }
+
+}
